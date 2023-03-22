b293aae083287115ce4de4a5c7d5aeb1dcee68ea
Justin Travis <justin@justintravis.com>
2023-03-22 19:36:10 +0000

    Move cart logic to custom hook

--- src/AddToCartButton/AddToCartButton.tsx
+++ src/AddToCartButton/AddToCartButton.tsx
@@ -1,53 +1,16 @@
 import React, { useState, useRef, useContext } from 'react'
 import { CartItem, Item } from '../models/store-models';
-import { CartContext } from '../CartContext';
-import { CartActionsContext } from '../CartActionsContext';
+import { useCart } from '../hooks/useCart';
 
 interface Props {
   item: Item
 }
 
 const AddToCartButton = ({ item }: Props) => {
-  const userCart = useContext(CartContext);
-  const setUserCart = useContext(CartActionsContext) as Function;
-
-  const addItemToCart = () => {
-    const itemInCart = userCart.find(userCartItem => {
-      return userCartItem.id === item.id;
-    });
-  
-    if (itemInCart) {
-      const newCart = userCart.map(cartItem => {
-        if (cartItem.id === item.id) {
-          const newQuantity = cartItem.quantity + 1;
-          return {
-            ...cartItem,
-            quantity: newQuantity,
-            totalPrice: newQuantity * cartItem.price
-          };
-        } else {
-          return cartItem;
-        }
-      }) as CartItem[];
-      setUserCart(newCart);
-    } else {
-      let cartItem: CartItem = {
-        id: item.id,
-        name: item.name,
-        quantity: 1,
-        price: item.price,
-        totalPrice: item.price
-      }
-  
-      setUserCart([
-        ...userCart,
-        cartItem
-      ]);
-    }
-  }
+  const { addItemToCart } = useCart();
 
   return (
-    <button onClick={ () => addItemToCart() }>Add to Cart</button>
+    <button onClick={ () => addItemToCart({ item }) }>Add to Cart</button>
   )
 }
 
---
+++ src/hooks/useCart.ts
@@ -0,0 +1,53 @@
+import { useState, useContext } from 'react';
+import { CartContext } from '../CartContext';
+import { CartActionsContext } from '../CartActionsContext';
+import { Item, CartItem } from '../models/store-models';
+
+interface Props {
+  item: Item
+}
+
+export function useCart() {
+  const userCart = useContext(CartContext);
+  const setUserCart = useContext(CartActionsContext) as Function;
+
+  const addItemToCart = ({ item }: Props) => {
+    const itemInCart = userCart.find(userCartItem => {
+      return userCartItem.id === item.id;
+    });
+  
+    if (itemInCart) {
+      const newCart = userCart.map(cartItem => {
+        if (cartItem.id === item.id) {
+          const newQuantity = cartItem.quantity + 1;
+          return {
+            ...cartItem,
+            quantity: newQuantity,
+            totalPrice: newQuantity * cartItem.price
+          };
+        } else {
+          return cartItem;
+        }
+      }) as CartItem[];
+      setUserCart(newCart);
+    } else {
+      let cartItem: CartItem = {
+        id: item.id,
+        name: item.name,
+        quantity: 1,
+        price: item.price,
+        totalPrice: item.price
+      }
+  
+      setUserCart([
+        ...userCart,
+        cartItem
+      ]);
+    }
+  }
+
+  return {
+    addItemToCart,
+    // removeItemFromCart
+  }
+}
