b081a829bbf2ba944016de6c5be2fa339f395a67
Justin Travis <justin@justintravis.com>
2023-03-22 15:47:49 +0000

    Add Suspense to ImageCarousel loading and some styles

--- src/ItemPDP/ItemPDP.tsx
+++ src/ItemPDP/ItemPDP.tsx
@@ -1,14 +1,29 @@
-import React, { useState, useRef, useContext } from 'react'
-import ImageCarousel from '../ImageCarousel/ImageCarousel';
+import React, { useState, useRef, useContext, Suspense, lazy } from 'react'
+// import ImageCarousel from '../ImageCarousel/ImageCarousel';
 import { CartContext } from '../CartContext';
 import { Item, CartItem } from '../models/store-models';
+
+import './ItemPDP.css';
 
 interface Props extends Item {
   handleAddItemToCart: Function;
   handleRemoveItemFromCart: Function;
 }
 
+const IMAGE_WIDTH = 250;
+const IMAGE_HEIGHT = 250;
+
+const Loader = () => {
+  // Make the loader the same size as the carousel so it
+  // doesn't cause the page to jump when the component actually loads
+  return (
+    <div className='PDP-Loader' style={{ width: IMAGE_WIDTH, height: IMAGE_HEIGHT + 80 }}>Loading images...</div>
+  )
+}
+
 const ItemPDP = ({ id, name, price, qtyAvailable, images, handleAddItemToCart }: Props) => {
+  const ImageCarousel = lazy(() => import('../ImageCarousel/ImageCarousel.js'));
+
   const cart: CartItem[] = useContext(CartContext); // Use context to display how many items are already in user's cart
 
   const getQtyInCart = () : number | undefined => {
@@ -20,7 +35,9 @@ const ItemPDP = ({ id, name, price, qtyAvailable, images, handleAddItemToCart }:
   return (
     <div>
       <h2>{ name }</h2>
-      <ImageCarousel images={ images } width={ 250 } height={ 250 } />
+      <Suspense fallback={ <Loader /> }>
+        <ImageCarousel images={ images } width={ IMAGE_WIDTH } height={ IMAGE_HEIGHT } />
+      </Suspense>
       <p>{ price }</p>
       { qtyAvailable > 0
         ? <button onClick={ () => handleAddItemToCart() }>Add to Cart</button>
